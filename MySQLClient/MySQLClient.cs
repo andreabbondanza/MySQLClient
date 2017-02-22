ddf1c0c8a772c4fc82ee028050f6e8a8f77962bf
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2017-02-22 17:36:25 +0100

    Some function, NOT WORKS

--- MySQLClient/MySQLClient.cs
+++ MySQLClient/MySQLClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,13 @@ using MySql.Data.MySqlClient;
 
 namespace DewCore.MySQLClient
 {
+    public class NoTransactionException : Exception 
+    {
+        public override string ToString()
+        {
+            return "No transaction initalized. Missing begin transaction";
+        }
+    }
     public interface IMySQLClient
     {
         /// <summary>
@@ -25,14 +33,6 @@ namespace DewCore.MySQLClient
         /// <param name="values">List of binded values</param>
         /// <returns>List of T objects (rows)</returns>
         Task<List<T>> QueryAsync<T>(string query, List<MySqlParameter> values);
-        /// <summary>
-        /// Perform a query
-        /// </summary>
-        /// <typeparam name="T">Result type object</typeparam>
-        /// <param name="query">Query</param>
-        /// <param name="values">List of binded values</param>
-        /// <returns>List of T objects (rows)</returns>
-        List<T> Query<T>(string query, List<MySqlParameter> values);
         /// <summary>
         /// Perform a select on a table
         /// </summary>
@@ -55,11 +55,19 @@ namespace DewCore.MySQLClient
         /// <param name="updated"></param>
         /// <returns></returns>
         Task<List<T>> UpdateAsync<T>(Func<Task, bool> selector, T updated);
-
-        Task<bool> CommitAsync();
-        Task<bool> RoolbackAsync();
-
-        Task<bool> BeginTransactionAsync();
+        /// <summary>
+        /// Commit a transaction
+        /// </summary>
+        Task CommitAsync();
+        /// <summary>
+        /// Rollback a transaction
+        /// </summary>
+        Task RoolbackAsync();
+        /// <summary>
+        /// Begin a transiction
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> BeginTransactionAsync(IsolationLevel isolationLevel);
 
     }
     public class MySQLClient : IMySQLClient, IDisposable
@@ -69,14 +77,48 @@ namespace DewCore.MySQLClient
         /// </summary>
         public readonly MySqlConnection Db = null;
         /// <summary>
+        /// Transaction var
+        /// </summary>
+        private MySqlTransaction transiction = null;
+        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="connectionString"></param>
         public MySQLClient(string connectionString)
         {
-            this.Db = new MySqlConnection(connectionString);
-            
+            this.Db = new MySqlConnection(connectionString);            
         }
+
+        public async Task<bool> BeginTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.Unspecified)
+        {
+            bool result = true;
+            try
+            {
+                if (this.transiction == null)
+                    this.transiction = await Db.BeginTransactionAsync(isolationLevel);
+            }
+            catch
+            {
+                result = false; 
+            }
+            return result;
+        }
+
+        public async Task CommitAsync()
+        {
+            if (transiction == null)
+                throw new NoTransactionException();
+            else
+            {
+                await this.transiction.CommitAsync();
+            }
+        }
+
+        public Task<List<T>> DeleteAsync<T>(Func<Task, bool> selector)
+        {
+            throw new NotImplementedException();
+        }
+
         /// <summary>
         /// Dispose client
         /// </summary>
@@ -98,6 +140,38 @@ namespace DewCore.MySQLClient
             
             return null;
 
+        }
+
+        public Task<List<object[]>> QueryArrayAsync<T>(string query, List<MySqlParameter> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> QueryAsync<T>(string query, List<MySqlParameter> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RoolbackAsync()
+        {
+            if (transiction == null)
+                throw new NoTransactionException();
+            else
+            {
+                await this.transiction.RollbackAsync();
+            }
+        }
+        public void p(object s,System.EventArgs a)
+        { }
+        public Task<List<T>> SelectAsync<T>(Func<Task, bool> selector)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> UpdateAsync<T>(Func<Task, bool> selector, T updated)
+        {
+            throw new NotImplementedException();
         }
     }
 }
